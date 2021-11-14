c040d4450e76b632de6c4da790cdd8a61629f1ff
Martin Klauco <martin@klauco.com>
2021-11-14 17:52:34 +0100

    favicon and few cards in dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app-coreui')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      
+      
+      <div class="row">
+        <div class="col-sm-6 col-lg-3">
+          @include('templates.card-block', ['counter' => $users, 'title' => 'Number of users', 'gradient' => 'primary'])
         </div>
+
+        <div class="col-sm-6 col-lg-3">
+          @include('templates.card-block', ['counter' => $books, 'title' => 'Number of books', 'gradient' => 'info'])
+        </div>
+
+        <div class="col-sm-6 col-lg-3">
+          @include('templates.card-block', ['counter' => $authors, 'title' => 'Number of authors', 'gradient' => 'danger'])
+        </div>
+
+        <div class="col-sm-6 col-lg-3">
+          @include('templates.card-block', ['counter' => 0, 'title' => 'n/a', 'gradient' => 'success'])
+        </div>
+
+      </div>
+      
+      
     </div>
+  </div>
 </div>
 @endsection
